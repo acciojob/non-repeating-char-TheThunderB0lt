90b27a24f18aa3f7fe038e8ca4fd3b7204e93b3a
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-04-02 22:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let count = {};
+
+	for ( let t of str) {
+		count = (count[t] || 0 ) + 1
+	}
+	for (let t of str){
+		if (count[t] === 1) {
+			return count;
+		}
+	}
+	
+	return null;
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// // alert(firstNonRepeatedChar(input)); 
